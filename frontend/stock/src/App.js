7fb3709dfc44f1019a0cffa2f172d8f8bf12d76a
Ashmit <ashmit[pandey04@gmail.com>
2025-10-27 10:45:19 +0530

    added authcontext for accessing protected endpoints

--- frontend/stock/src/App.js
+++ frontend/stock/src/App.js
@@ -6,18 +6,23 @@ import Footer from './components/Footer';
 import Button from './components/Button';
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Register from './components/Register';
+import Login from './components/Login';
+import AuthProvider from './AuthProvider';
 function App() {
   return (
   <>
+    <AuthProvider>
      <BrowserRouter >
      <Header />
         <Routes>
           <Route path='/' element={<Main />} />
           <Route path='/register' element={<Register />} />
+          <Route path='/login' element={<Login />} />
         </Routes>
       <Footer />
       <Button />
       </BrowserRouter>
+    </AuthProvider>
     
   </>
   )
