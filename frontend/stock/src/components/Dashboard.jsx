20026f4d7a425cffc6afeaf0470773f6f38c500e
Ashmit <ashmit[pandey04@gmail.com>
2025-12-05 18:27:37 +0530

    integrated post api

--- frontend/stock/src/components/Dashboard.jsx
+++ frontend/stock/src/components/Dashboard.jsx
@@ -4,6 +4,12 @@ import axiosInstance from './../axiosInstance'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSpinner } from '@fortawesome/free-solid-svg-icons'
 const Dashboard = () => {
+    const [ticker, setTicker] = useState('')
+    const [error, setError] = useState()
+    const [loading, setLoading] = useState(false)
+    const [plot, setPlot] = useState()
+    const [ma100, setMA100] = useState()
+    const [ma200, setMA200] = useState()
    useEffect(()=>{
         const fetchProtectedData = async () =>{
             try{
@@ -14,9 +20,83 @@ const Dashboard = () => {
         }
         fetchProtectedData();
     }, [])
+    const handleSubmit = async (e) =>{
+        e.preventDefault()
+        setLoading(true)
+        try{
+          const response = await axiosInstance.post('/predict/',{ticker: ticker})
+          const plotUrl = response.data.plot_img
+          const ma100Url = response.data.plot_100_dma
+          const ma200Url = response.data.plot_200_dma
+          const predictionUrl = response.data.plot_prediction
+          setPlot(plotUrl)
+          setMA100(ma100Url)
+          setMA200(ma200Url)
+
+          if (response.data.error){
+            setError(response.data.error)
+          }
+        }
+        catch(error){
+            setError('An error occurred. Please try again.')
+        }
+        finally{
+            setLoading(false)
+        }
+
+    }
   return (
-    <div>Dashboard</div>
-  )
+  <div className='container'>
+    <div className="row">
+      <div className="col-md-6 mx-auto">
+        <form onSubmit={handleSubmit}>
+          <input 
+            type="text" 
+            className='form-control' 
+            placeholder='Enter Stock Ticker'
+            onChange={(e) => setTicker(e.target.value)} 
+            required
+          />
+
+          {error && (
+            <small className='text-danger'>{error}</small>
+          )}
+
+          <button type='submit' className='btn btn-info mt-3'>
+            {loading ? (
+              <span>
+                <FontAwesomeIcon icon={faSpinner} spin /> Please wait...
+              </span>
+            ) : (
+              'See Prediction'
+            )}
+          </button>
+        </form>
+      </div>
+    </div>
+
+    {/* Print prediction plots */}
+      <div className="prediction mt-5">
+        <div className="p-3">
+          {plot && (
+            <img src={plot} style={{ maxWidth: '100%' }} alt="Prediction Plot" />
+          )}
+        </div>
+         <div className="p-3">
+                    {ma100 && (
+                        <img src={ma100} style={{ maxWidth: '100%' }} />
+                    )}
+                </div>
+          <div className="p-3">
+                    {ma200 && (
+                        <img src={ma200} style={{ maxWidth: '100%' }} />
+                    )}
+                </div>
+
+            
+      </div>
+  </div>
+);
 }
 
 export default Dashboard
